61fa5edb27c7dfb7a6a43dc1daeab067046be23a
12haa <mkhaefi2500@yahoo.com>
2023-06-04 12:21:31 -0700

    Added Data Fetching

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./modules/Home/index";
-
+//  Watched Till 00:20:13
 function App() {
   return (
     <div className="text-gray-600 text-3xl">
--- src/modules/Home/index.jsx
+++ src/modules/Home/index.jsx
@@ -1,23 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Hero from "../../components/Hero/Hero";
 import Products from "../../components/Products/Products";
 import Feature from "../../components/Feature/Feature";
+import StatCard from "../../components/StatCard/StatCard";
+import Footer from "../../components/Footer/Footer";
 
 const index = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <div>
       <Hero />
-      <div class="flex flex-col text-center w-full mb-20">
-        <h2 class="text-xs text-indigo-400 tracking-widest font-medium title-font mb-1">
+      <div className="flex flex-col text-center w-full ">
+        <h2 className="text-xs text-indigo-400 tracking-widest font-medium title-font pb-3 text-[23px]  bg-[#111827]">
           Prodcuts
         </h2>
-        <h1 class="sm:text-3xl text-2xl font-medium title-font text-white">
+        <h1 className="sm:text-3xl text-2xl font-medium title-font text-white bg-[#111827]">
           Most Popular Products
         </h1>
       </div>
+      {products.length > 0 ? (
+        <Products products={products} />
+      ) : (
+        <div className="bg-[#111827]  border-2 pl-[730px] font-mono font-semibold w-[100%] pt-10 text-white">
+          Loading Data ..
+        </div>
+      )}
 
       <Products />
       <Feature />
+      <StatCard />
+      <Footer />
     </div>
   );
 };
